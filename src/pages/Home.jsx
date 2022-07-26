a059bd632d9b7757630c38eddc5f0a4f66e5f0a4
Ritam <iwritecode000@gmail.com>
2022-07-26 15:06:11 +0530

    CheckOutVideos & Contact form added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,8 @@ import Caraosel from '../components/Caraosel';
 import About from '../components/About';
 import Cards from '../components/Cards';
 import Gallery from '../components/Gallery';
+import CheckOutVideos from '../components/CheckOutVideos';
+import ContactForm from '../components/ContactForm';
 
 const Home = () => {
   return (
@@ -11,6 +13,8 @@ const Home = () => {
     <About/>
     <Cards/>
     <Gallery/>
+    <CheckOutVideos/>
+    <ContactForm/>
     </>
   )
 }
