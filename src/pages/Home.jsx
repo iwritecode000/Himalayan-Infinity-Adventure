703e23158fcf2951b8bf6d39dca2c157609d417a
Ritam <iwritecode000@gmail.com>
2022-07-26 14:50:16 +0530

    Cards section added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Caraosel from '../components/Caraosel';
 import About from '../components/About';
+import Cards from '../components/Cards';
 
 const Home = () => {
   return (
     <>
     <Caraosel/>
     <About/>
+    <Cards/>
     </>
   )
 }
